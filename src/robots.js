de8777a0420bc196f463b939e92380a8483fbde6
Siim-kodu <siim.pwn@gmail.com>
2018-05-29 20:31:50 +0300

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Edgar Savisaar',
+      username: 'Ets',
+      email: 'edgar@kesk.ee'
+    },
+    {
+      id: 2,
+      name: 'Toomas H Ilves',
+      username: 'Tom',
+      email: 'toomas@president.tv'
+    },
+    {
+      id: 3,
+      name: 'Jürgen Ligi',
+      username: 'Jürks',
+      email: 'jurgen@ref.net'
+    },
+    {
+      id: 4,
+      name: 'Siim Kallas',
+      username: 'Kallas',
+      email: 'rahad@kadunud.ru'
+    },
+    {
+      id: 5,
+      name: 'Kersti Kaljurand',
+      username: 'Kalju',
+      email: 'kersti@hot.ee'
+    },
+    {
+      id: 6,
+      name: 'Naise Nimi',
+      username: 'blondu',
+      email: 'mingi_mail@mail.info'
+    },
+    {
+      id: 7,
+      name: 'Siim Saar',
+      username: 'Siim',
+      email: 'siim.pwn@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'Lokk Klokk',
+      username: 'Lock',
+      email: 'lockd@mail.me'
+    },
+    {
+      id: 9,
+      name: 'Andres Laar',
+      username: 'Ants',
+      email: 'andres@rate.io'
+    },
+    {
+      id: 10,
+      name: 'Joonas Nilp',
+      username: 'Joonas',
+      email: 'joonas@hot.ee'
+    }
+  ];
+  
